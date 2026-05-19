b1048307e12a0c3843a2d0de2a97b9c5d8596184
Alexey Krasnov <alex.krasnov.2006@gmail.com>
2026-05-19 13:15:35 +0300

    [test] vfs simple test

--- include/test/test.h
+++ include/test/test.h
@@ -1,11 +1,23 @@
 #ifndef TEST_H
 #define TEST_H
 
+#include <lib/stdio.h>
 #include <stddef.h>
 #include <stdint.h>
+
+#define ASSERT_TEST(msg, condition)                           \
+	do {                                                      \
+		if (condition) {                                      \
+			printf("[OK]: %s\n", msg);                        \
+		} else {                                              \
+			printf("[FAIL]: %s (Line: %d)\n", msg, __LINE__); \
+			return;                                           \
+		}                                                     \
+	} while (0)
 
 void test_mm();
 void test_ramfs();
 void test_ramfs_creation();
+void test_vfs_layer();
 
 #endif
