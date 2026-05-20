19d48d1941e585acbb44be9a9ef98f5a5113a72c
Alexey Krasnov <alex.krasnov.2006@gmail.com>
2026-05-20 12:46:41 +0300

    [feat] GRUB bootloader, const kernel size, gdt loading

--- init/kernel_init.c
+++ init/kernel_init.c
@@ -1,3 +1,4 @@
+#include <arch/gdt.h>
 #include <arch/isr.h>
 #include <drivers/screen.h>
 #include <fs/ramfs.h>
@@ -7,14 +8,18 @@
 #include <stdint.h>
 #include <test/test.h>
 
-void kernel_main() {
+void kernel_main(unsigned long magic, unsigned long addr) {
+	if (magic != 0x2BADB002) {
+		return;
+	}
+	init_gdt();
 	isr_install();
 	irq_install();
 	clear_screen();
 	init_paging();
 	clear_screen();
-	test_ramfs();
-	test_ramfs_creation();
+	// vfs_init();
+	test_vfs_layer();
 
 	printf("Type something... END to halt the CPU\n> ");
 }
